8ef8958bdfd18e9c6f7c6e03bee0854664c34031
apeterso <apeterso@student.42paris.fr>
2026-06-24 18:46:29 +0200

    feat(render): fixed wall render to be center and not switching place when viewing from front or back and huge refactoring

--- Includes/Rendering/render.h
+++ Includes/Rendering/render.h
@@ -14,4 +14,19 @@ void	render_enemy(t_game *game, t_img *img);
 int		render(t_game *game);
 // void	render_doors(t_ray *ray, t_img *image);
 
+typedef struct s_sprite_draw
+{
+	int				cur_x;
+	int				cur_y;
+	int				height;
+	int				width;
+	int				start_x;
+	int				start_y;
+	double			tex_x;
+	double			tex_y;
+	double			tex_step_x;
+	double			tex_step_y;
+}	t_sprite_draw;
+
+
 #endif
